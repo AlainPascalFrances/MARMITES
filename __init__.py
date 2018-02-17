b5b2564368433c1594eaff4edaced38a7525dd22
Alain Pascal Francés <frances.alain@gmail.com>
2018-02-17 10:40:20 +0000

    Added PTH files

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""
+MARMITES
+"""
+
+__name__ = 'MARMITES'
+__author__ = 'Alain P. Frances'
+from .version import __version__, __build__, __git_commit__
+
+#imports
+#from . import modflow
+#from . import mt3d
+#from . import seawat
+#from . import modpath
+#from . import modflowlgr
+#from . import utils
+#from . import plot
+#from . import export
+#from . import pest
+#from . import mf6
+#from .mbase import run_model, which, is_exe
